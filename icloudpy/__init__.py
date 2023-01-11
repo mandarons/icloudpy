1ca39d6d958322ce2b5a63a04f93826a6bb605e1
Mandar Patil <mandarons@pm.me>
2023-01-11 14:59:59 +0000

    Fixed failing tests

--- icloudpy/__init__.py
+++ icloudpy/__init__.py
@@ -1,4 +1,6 @@
 """The iCloudPy library."""
 import logging
 
+from icloudpy.base import ICloudPyService  # pylint: disable=unused-import
+
 logging.getLogger(__name__).addHandler(logging.NullHandler())
--- icloudpy/services/__init__.py
+++ icloudpy/services/__init__.py
@@ -1,1 +1,13 @@
 """Services."""
+from icloudpy.services.account import AccountService  # pylint: disable=unused-import
+from icloudpy.services.calendar import CalendarService  # pylint: disable=unused-import
+from icloudpy.services.contacts import ContactsService  # pylint: disable=unused-import
+from icloudpy.services.drive import DriveService  # pylint: disable=unused-import
+from icloudpy.services.findmyiphone import (  # pylint: disable=unused-import
+    FindMyiPhoneServiceManager,
+)
+from icloudpy.services.photos import PhotosService  # pylint: disable=unused-import
+from icloudpy.services.reminders import (  # pylint: disable=unused-import
+    RemindersService,
+)
+from icloudpy.services.ubiquity import UbiquityService  # pylint: disable=unused-import
